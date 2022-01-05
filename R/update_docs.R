69214730bb210fcfb5202af6a0fa074b41801b0d
dieghernan <diego.hernangomezherrero@gmail.com>
2022-01-05 17:12:59 +0100

    not write minimeta

--- R/update_docs.R
+++ R/update_docs.R
@@ -121,7 +121,7 @@ update_docs <- function(pkg = ".",
   if (create_codemeta) {
     if (verbose) cat(crayon::green("Creating codemeta\n"))
 
-    codemetar::write_codemeta(write_minimeta = TRUE)
+    codemetar::write_codemeta()
   }
   if (create_cff) {
     if (verbose) cat(crayon::green("Creating CITATION.cff\n"))
