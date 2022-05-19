b67f2fb70804dbf7ab25fa9f13ad7be61f7c8f9d
dieghernan <dieghernan@users.noreply.github.com>
2022-05-19 12:56:58 +0000

    Update docs with pkgdev

--- R/add_global_gitgnore.R
+++ R/add_global_gitgnore.R
@@ -79,7 +79,7 @@ add_global_gitgnore <- function(pkg = ".") {
 
   usethis::use_build_ignore("CRAN-SUBMISSION")
   usethis::use_git_ignore("CRAN-SUBMISSION", directory = pkg)
-  
+
   usethis::use_build_ignore("cran-comments.md")
   usethis::use_git_ignore("cran-comments.md", directory = pkg)
 
